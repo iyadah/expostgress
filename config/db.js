e6302fa1b6744b3da6eca7b47b4dc3a1df9b9699
Iyad Ahmad <iyad.ahmad@gmail.com>
2021-08-28 11:19:45 +0300

    pool

--- config/db.js
+++ config/db.js
@@ -1,22 +1,26 @@
 const Pool = require("pg").Pool;
 const config = require("config");
-
+var pool;
 const connectDB = async () => {
   try {
-    const pool = await new Pool({
+    pool = await new Pool({
       user: config.user,
       host: config.host,
       database: config.database,
       password: config.password,
       port: config.port,
     });
+    console.log(
+      "pool:" + (await pool.query("select * from auth_user limit 10")).rowCount
+    );
     console.log("Postgres connected..");
   } catch (err) {
     console.error(err.message);
     process.exit(1);
   }
 };
-
+console.log("pool before exports: " + pool);
 module.exports = {
   connectDB,
+  pool,
 };
--- index.js
+++ index.js
@@ -1,8 +1,50 @@
 const express = require("express");
-const connectDBES = require("./config/db");
+const Pool = require("pg").Pool;
+const config = require("config");
+
+// const connectDB = require("./config/db");
 var cors = require("cors");
 const app = express();
-connectDBES.connectDB();
+var pool;
+const connectDB = async () => {
+  try {
+    pool = await new Pool({
+      user: config.user,
+      host: config.host,
+      database: config.database,
+      password: config.password,
+      port: config.port,
+    });
+    console.log(
+      "pool:" + (await pool.query("select * from auth_user limit 10")).rowCount
+    );
+    console.log("Postgres connected..");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+connectDB();
+// try {
+//   const getUsers = (request, response) => {
+//     console.log("query");
+
+//     connectDB.pool.query(
+//       "SELECT * FROM auth_user ORDER BY id asc limit 10",
+//       (error, results) => {
+//         if (error) {
+//           throw error;
+//         }
+//         console.log(results.rows);
+//       }
+//     );
+//   };
+//   getUsers();
+
+// } catch (err) {
+//   console.error(err.message);
+//   res.status(500).send("Server error");
+// }
 app.get("/", (req, res) => res.send("API RUNNING"));
 app.use(express.json({ extended: false }));
 app.use(
@@ -11,7 +53,7 @@ app.use(
   })
 );
 
-app.use("/api/pg", require("./routes/api/pg"));
+//app.use("/api/pg", require("./routes/api/pg"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started in port ${PORT}`));
